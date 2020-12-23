453eb4df458a2c1bd8469fd2fd778164b9cc446c
kess531 <67779553+kess531@users.noreply.github.com>
2020-12-23 22:11:22 +0900

    JS: HomeOpacity

--- main.js
+++ main.js
@@ -2,30 +2,29 @@
 
 
 // 2020.12.07 Navbar 투명
-const navbar = document.querySelector('#navbar');
-const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll',()=>{  //스크롤이 될때마다 등록한 함수를 호출
+    const navbar = document.querySelector('#navbar');
+    const navbarHeight = navbar.getBoundingClientRect().height;
 
+    document.addEventListener('scroll',()=>{  //스크롤이 될때마다 등록한 함수를 호출
     if(window.scrollY>navbarHeight){
         navbar.classList.add('navbar--dark'); //스크롤값이 navbarHeight값 보다 크면 #navbar에 class 추가
     }
     else{
         navbar.classList.remove('navbar--dark'); //스크롤값이 navbarHeight값 보다 작으면 #navbar에 class 제거
     }
-})
+});
 
 
 //2020.12.23 scollbar 이동
 
 const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click',(event)=>{
 
+navbarMenu.addEventListener('click',(event)=>{
     const target = event.target;
     const link = event.target.dataset.link;
     if(link == null){
         return;
     }
-
     scrollIntoView(link);
 });
 
@@ -33,6 +32,20 @@ const contackbtn = document.querySelector('.home__contact');
     contackbtn.addEventListener('click', ()=>{
     scrollIntoView('#Contack');
 });
+
+
+
+//2020.12.23 scollbar 이동시 홈 투명화
+const home = document.querySelector('.home__container');
+const homeHeight =  home.getBoundingClientRect().height;
+
+document.addEventListener('scroll',()=>{
+    home.style.opacity = 1 -  window.scrollY / homeHeight;
+});
+
+
+
+
 
 
 function scrollIntoView(selector){
