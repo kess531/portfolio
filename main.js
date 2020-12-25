2d7deada1aa49e779adbfc28332999b619d1d241
kess531 <67779553+kess531@users.noreply.github.com>
2020-12-25 18:35:40 +0900

    JS: Final Touch

--- main.js
+++ main.js
@@ -15,7 +15,9 @@
 });
 
 
-//2020.12.23 scollbar 이동
+
+
+//2020.12.23 scrollbar 이동
 
 const navbarMenu = document.querySelector('.navbar__menu');
 const navbarmenuitem = document.querySelector('.navbar__menu__item');
@@ -27,6 +29,7 @@ navbarMenu.addEventListener('click',(e)=>{
     }
     navbarMenu.classList.remove('open');
     scrollIntoView(link);
+    selectNavItem(target);
     
 });
 
@@ -66,8 +69,66 @@ arrowup.addEventListener('click',()=>{
 
 
 
+
+
+
+
+// 엣지케이스
+const sectionIds= ['#Home','#About','#Skills','#Work','#Contack'];
+
+
+
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+
+let selectedNavIndex =0;
+let selectedNavItem = navItems[0];
+function selectNavItem(seleted){
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = seleted;
+    selectedNavItem.classList.add('active');
+}
+
+const observerOption= {
+    root: null,
+    rootmargin:'0px',
+    threshold: 0.3,
+}
+const observerCallback = (entries, observer)=>{
+    entries.forEach(entry => {
+        if(!entry.isIntersecting && entry.intersectionRatio > 0){
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            
+            // y<0 이면 스크롤링이 아래로 내려감
+            if(entry.boundingClientRect.y < 0){
+                selectedNavIndex = index + 1;
+            }else{
+                selectedNavIndex = index - 1;
+            }
+            
+        }
+    });
+};
+const observer = new IntersectionObserver(observerCallback,observerOption);
+
+sections.forEach(section =>observer.observe(section));
+
+window.addEventListener('wheel',()=>{ //wheel : 사용자가 스스로 스크롤할때 
+    if(window.scrollY===0)
+    {
+        selectedNavIndex = 0;
+    }
+    else if(Math.round(window.scrollY + window.innerHeight) >=
+    document.body.clientHeight){
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+});
+
+
 //스크롤 이동 함수
 function scrollIntoView(selector){
     const scrollTo=document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
-}
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
+};
